2b26a9417c6f53c6fae9900420ebf5a68c25e5dd
Shweta <shvetacrane@gmail.com>
2019-03-07 23:55:15 -0600

    testing and added  routes for CSS

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set('view engine', 'hbs');
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(process.cwd() + "/public"));
 app.use("/", index);
 app.use("/user", user);
 app.use("/race", race);
