912103aa16860c2a97a305f38d148fb0c2210f8a
Elizabeth Phillips <ELIZABETH_PHILLIPS@homedepot.com>
2019-03-18 23:43:13 -0500

    Created the search bar results for running groups and races

--- routes/race.js
+++ routes/race.js
@@ -2,15 +2,51 @@ const express = require("express");
 const router = express.Router();
 const {Race} = require("../db/ready_race_run");
 
+function getStates(races){
+  states = []
+
+  for (let i = 0; i < races.length; i++){
+    states.push({id: races[i].id, s: races[i].state})
+  }
+  return states
+}
+
+function getFiltered(results, query, state){
+  if (state == "All" && query.trim() == ""){
+    return results
+  } 
+  output = []
+  for (let i = 0; i < results.length; i++){
+    console.log(typeof results[i].name)
+    if ((state != "All" && results[i].state == state)||results[i].name.includes(query)){
+      output.push(results[i])
+    }
+  }
+  return output
+}
+
 router.get("/", (req, res) => {
      Race.fetchAll()
     .then( Race => {
-        res.status(200).render('viewraces', { data: JSON.parse(JSON.stringify( Race)) });
+      result = JSON.parse(JSON.stringify( Race))
+      res.status(200).render('viewraces', { data: getFiltered(result, "", "All"), states: getStates(result) });
     })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.post("/", (req, res) => {
+  Race.fetchAll()
+  .then( races => {
+    result = JSON.parse(JSON.stringify( races))
+    res.status(200).render('viewraces', { data: getFiltered(result, req.body.query, req.body.selectpicker), states: getStates(result) });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post("/update/:id", (req, res) => {
--- routes/running_group.js
+++ routes/running_group.js
@@ -2,16 +2,54 @@ const express = require("express");
 const router = express.Router();
 const {RunningGroup} = require("../db/ready_race_run");
 
+function getStates(runningGroups){
+  states = []
+
+  for (let i = 0; i < runningGroups.length; i++){
+    states.push({id: runningGroups[i].id, s: runningGroups[i].state})
+  }
+  return states
+}
+
+function getFiltered(results, query, state){
+  if (state == "All" && query.trim() == ""){
+    return results
+  } 
+  output = []
+  for (let i = 0; i < results.length; i++){
+    console.log(typeof results[i].name)
+    if ((state != "All" && results[i].state == state)||results[i].name.includes(query)){
+      output.push(results[i])
+    }
+  }
+  return output
+}
+
 router.get("/", (req, res) => {
-    RunningGroup.fetchAll()
-    .then(RunningGroup => {
-        res.status(200).render('allrunninggroups', { data: JSON.parse(JSON.stringify(RunningGroup)) });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+  RunningGroup.fetchAll()
+ .then( RunningGroup => {
+   result = JSON.parse(JSON.stringify( RunningGroup))
+   res.status(200).render('allrunninggroups', { data: getFiltered(result, "", "All"), states: getStates(result) });
+ })
+ .catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+ });
 });
+
+router.post("/", (req, res) => {
+  RunningGroup.fetchAll()
+  .then( runningGroups => {
+    result = JSON.parse(JSON.stringify( runningGroups))
+    res.status(200).render('allrunninggroups', { data: getFiltered(result, req.body.query, req.body.selectpicker), states: getStates(result) });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+ 
 
 router.get("/create/", (req, res) => {
     res.render('admin/createrunninggroup')
