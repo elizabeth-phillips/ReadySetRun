064cdbdfed452f62d0511d0f6b8901b5dcf2bf4c
Elizabeth Phillips <ELIZABETH_PHILLIPS@homedepot.com>
2019-04-25 21:09:12 -0400

    Added the recommended races

--- routes/data/raceData.js
+++ routes/data/raceData.js
@@ -2,6 +2,80 @@ const axios = require('axios');
 const knex = require('knex')(require('../../knexfile')[process.env.NODE_ENV]);
 const fs = require('fs');
 
+function abbrState(input, to){
+    
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arizona', 'AZ'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+
+    if (to == 'abbr'){
+        input = input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        for(i = 0; i < states.length; i++){
+            if(states[i][0] == input){
+                return(states[i][1]);
+            }
+        }    
+    } else if (to == 'name'){
+        input = input.toUpperCase();
+        for(i = 0; i < states.length; i++){
+            if(states[i][1] == input){
+                return(states[i][0]);
+            }
+        }    
+    }
+}
+
 function getDateTime() {
     var date = new Date();
     var year = date.getFullYear();
@@ -80,6 +154,37 @@ async function getSingleRaceDetail(name){
     }
 }
 
+
+async function getRecommendation(user){
+    console.log("Finding recommendations: ")
+    try{
+        var API_KEYV2 = 'esdnh7utmgwp82uj9qwgktyp';
+        let response = '';
+        let results = '';
+
+        state = abbrState(user.state, 'abbr')
+
+        console.log("MY STATE", state)
+        
+        response = await axios.get(`http://api.amp.active.com/v2/search?query=running&category=event&country=United%20States&start_date=${getDateTime()}..2019-12-31&api_key=${API_KEYV2}&exists=asset.assetName&per_page=10&state=${state}`);
+        results = response.data.results;
+
+        let responses = []
+        for(let i = 0; i < results.length; i++){
+            temp = resultFormat(results[i])
+
+            if(Object.entries(temp).length !== 0){
+                responses.push(temp);
+            }
+        }
+        responses = sortDates(responses).slice(0,3)
+        console.log("Finding recommendations: ", responses)
+        return responses;
+    } catch (error){
+        console.log(error);
+    }
+}
+
 async function getRaceDetails(state, query){
     try{
         var API_KEYV2 = 'esdnh7utmgwp82uj9qwgktyp';
@@ -143,5 +248,6 @@ async function isPartOf(user_id, race_name){
 module.exports = {
     RaceDetails: getRaceDetails,
     SingleRace: getSingleRaceDetail,
-    IsPartOf: isPartOf
+    IsPartOf: isPartOf,
+    Recommendation: getRecommendation
 }
