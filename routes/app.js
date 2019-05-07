f19dfb8722711cca8f857c535160f6d554ae999d
Shweta <shvetacrane@gmail.com>
2019-05-07 09:11:57 -0500

    added test coverage

--- routes/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const router = express.Router();
-
-app.get('/', (req, res) => {
-    res.status(200);
-  });
-  //testing github account
-module.exports = app
